7e6fcdc9c8580fca853cd3794734588d8d5be81f
hernaval <hernavalasco@gmail.com>
2020-12-16 23:56:26 +0300

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -99,7 +99,7 @@ class UserController extends Controller
         return response()->json([
             'message' => "user registered",
             "data" => $newUser
-        ]);
+        ]);//
 
     }
 
