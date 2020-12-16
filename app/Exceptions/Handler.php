2d8d033ec3681aeda8af17b22f6373b3579d3f61
hernaval <hernavalasco@gmail.com>
2020-12-17 00:21:35 +0300

    add custom exception message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,4 +52,9 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'unauthenticated'], 401);
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
     }
 
     public function index(){
