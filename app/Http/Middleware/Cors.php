03511e89defff5c37394ba54b0fad391e0da9243
hernaval <hernavalasco@gmail.com>
2020-12-17 07:22:22 +0300

    change

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,15 +15,15 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-          header('Acess-Control-Allow-Origin: *');
+        //  header('Acess-Control-Allow-Origin: *');
         //  header('Acess-Control-Allow-Headers: *');
         //  header('Acess-Control-Allow-Origin: Content-type, X-Auth-Token, Authorization, Origin');
         //  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
     
          return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'POST,GET, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers','Accept, Origin, Content-Type,X-Requested-With, X-Auth-Token,Authorization');
+            // ->header('Access-Control-Allow-Origin', '*')
+            // ->header('Access-Control-Allow-Methods', 'POST,GET, PUT, DELETE, OPTIONS')
+            // ->header('Access-Control-Allow-Headers','Accept, Origin, Content-Type,X-Requested-With, X-Auth-Token,Authorization');
        
     }
 }
