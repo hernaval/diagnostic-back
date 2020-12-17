b63dc1c0260b9901fa83108df59df5cf3653b862
hernaval <hernavalasco@gmail.com>
2020-12-17 05:49:21 +0300

    hange

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://api.diagnosticcom.lech0958.odns.fr'],
 
     'allowed_origins_patterns' => [],
 
