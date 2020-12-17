c3d5c2c861eb97fd26ad4563366efcba53c9e67f
hernaval <hernavalasco@gmail.com>
2020-12-17 04:18:51 +0300

    fix cors issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,8 +16,6 @@ $app = new Illuminate\Foundation\Application(
 );
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorizations');
 
 /*
 |--------------------------------------------------------------------------
