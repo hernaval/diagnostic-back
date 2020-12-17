3ab00f59b0bdd73fa598a175fd6f29e6f153e711
hernaval <hernavalasco@gmail.com>
2020-12-17 07:11:29 +0300

    change

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -22,7 +22,7 @@ class Cors
     
          return $next($request)
            // ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'POST,GET, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers','Accept, Origin, Content-Type,X-Requested-With, X-Auth-Token,Authorization');
        
     }
