d7b440ec6246dcf967e1a2036f432a033ae6d879
hernaval <hernavalasco@gmail.com>
2020-12-17 06:01:46 +0300

    hange

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
+        'guest' => \App\Http\Middleware\Cors::class,
         
     ];
 }
