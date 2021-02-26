6583c09308625ea5ff899adabbe331c197da2509
hernaval <hernavalasco@gmail.com>
2021-02-26 11:12:04 +0300

    test reset

--- resources/views/emails/resetSuccess.blade.php
+++ resources/views/emails/resetSuccess.blade.php
@@ -2,28 +2,46 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Réinialisation réussi</title>
+    <title><title>Réinialisation réussi</title></title>
 </head>
-<body>
-    <div >
-        <div >
-            <img src="" width="100" alt="">
-        </div>
+<body style="background-color: #f9f9f9; font-family: Open Sans, Helvetica Neue, sans-serif!important;">
+    <img style="  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+  width: 40%;" src="E:\acadis.png" alt="">
 
-        <div>
-     
-   
-       
-        <p >
-           Félicitations ! Votre mot de passe a été réinitialisé. Vous pouver maintenant vous reconnecter.
-        </p>
-        <p>Suivez: <a href="http://diagnosticcom.lech0958.odns.fr/"> Se connecter</a>  </p>
-            
-        <p>Merci.</p>
-        </div>
+    <div style="    text-align: center;
+    width: 40%;
+    margin-left: 25%;
+    background-color: white;
+    border-radius: 5px;
+    margin-top: 20px;    
+    margin-bottom: 40px;
+    padding: 40px 80px 40px 80px;
+">
+        <h1>Activer votre compte</h1>
+        <p  style="margin-top: 40px;">Bonjour, Jean pierre</p>
+        <p  style="margin-top: 20px;">Utiliser le code ci-dessous pour activer votre compte</p>
+        <p style="    margin-top: 20px;
+        margin-bottom: 20px;
+        font-weight: bold;
+        color: #117abe;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+        font-size: 36px !important;">199955</p>
+        <p  style="margin-top: 40px;">Merci,</p>
+        <p>Groupe Acadys</p>
 
-        <div></div>
     </div>
+    
+    
+    <div style="text-align: center; margin-top: 20px; margin-bottom: 50px">
+    <p>Nous contacter: 00 000 00 000  |  example@gmail.com</p>
+    <p style="margin-top: 20px;  
+">Site: <a style="text-decoration: none!important;" href="acadys.com">acadys.com</a></p></div>
 </body>
-</html>
+</html> 
