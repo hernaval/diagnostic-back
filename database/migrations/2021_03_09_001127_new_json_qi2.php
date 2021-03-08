76927cc5560e5ff8f9848c5ac2528a5b465a1585
Hernaval <hernavalasco@gmail.com>
2021-03-09 00:12:10 +0300

    new migration jsonQuestionnaire

--- database/migrations/2021_03_09_001127_new_json_qi2.php
+++ database/migrations/2021_03_09_001127_new_json_qi2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class NewJsonQi extends Migration
+class NewJsonQi2 extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class NewJsonQi extends Migration
      */
     public function up()
     {
-        Schema::table('TQ', function (Blueprint $table) {
+        Schema::table('TQuestionnaire', function (Blueprint $table) {
             $table->string("jsonQuestionnaire")->nullable();
         });
     }
