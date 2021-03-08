7fdda53be55041fb09a0e7a6d7e8bfde4c398d4b
Hernaval <hernavalasco@gmail.com>
2021-03-08 23:56:50 +0300

    missing DB facades

--- app/Http/Controllers/Admin/DimensionController.php
+++ app/Http/Controllers/Admin/DimensionController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Model\TDimension;
-
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class DimensionController extends Controller
