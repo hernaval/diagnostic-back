afc7c8938fd0293442a68c1d78b13a4aee578cba
Hernaval <hernavalasco@gmail.com>
2021-03-08 23:39:52 +0300

    use validator forgotten

--- app/Http/Controllers/Admin/MesureController.php
+++ app/Http/Controllers/Admin/MesureController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Model\TDimension;
 use Illuminate\Support\Facades\DB;
-
+use Validator;
 
 class MesureController extends Controller
 {
--- app/Http/Controllers/Admin/QuestionnaireController.php
+++ app/Http/Controllers/Admin/QuestionnaireController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Model\TQuestionnaire;
 use Illuminate\Support\Facades\DB;
-
+use Validator;
 
 
 class QuestionnaireController extends Controller
