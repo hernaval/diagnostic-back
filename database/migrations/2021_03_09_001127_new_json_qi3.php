d8f91f829e710cadcc994f561ce123bcde7ec3da
Hernaval <hernavalasco@gmail.com>
2021-03-09 20:05:54 +0300

    migrate json text

--- database/migrations/2021_03_09_001127_new_json_qi3.php
+++ database/migrations/2021_03_09_001127_new_json_qi3.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class NewJsonQi2 extends Migration
+class NewJsonQi3 extends Migration
 {
     /**
      * Run the migrations.
