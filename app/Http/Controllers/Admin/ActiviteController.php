473ddd4371fcbe05338a4d7925fe27cfaeae49ba
Hernaval <hernavalasco@gmail.com>
2021-03-10 18:56:39 +0300

    user activity in admin

--- app/Http/Controllers/Admin/ActiviteController.php
+++ app/Http/Controllers/Admin/ActiviteController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use \App\Model\Activity;
 
 class ActiviteController extends Controller
 {
-    
+    public function indexUser($id)
+    {
+        return response()->json(
+            Activity::where(['userId' => $id])->get()
+        );
+    }
 }
